1bbb673c21aa906d7bdf016db9e790ce3d306787
vishnumohanrk <mohanvish1999@gmail.com>
2020-12-08 22:24:26 +0530

    modify colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bgPrimary: '#282c35',
-        bgSecondary: '#363c48',
-        textPrimary: 'hsla(0, 0%, 100%, 0.88)',
-        textSecondary: '#fff',
-        accent: '#ffa7c4',
+        bgFull: '#0c111b',
+        bg2: '#192133',
+        bg3: '#030b17',
+        bg4: '#000000',
+        textNormal: 'rgba(255, 255, 255, 0.8)',
+        textHighlight: '#ffffff',
+        inputPlaceholder: 'rgba(255, 255, 255, 0.6)',
+        inputBorder: 'rgba(255, 255, 255, 0.6)',
+        accent: '#1f80e0',
       },
     },
   },
