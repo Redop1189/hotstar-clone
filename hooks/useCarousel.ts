fa5f7fd3aafdc87039d6315222397923da154c59
vishnumohanrk <mohanvish1999@gmail.com>
2020-12-29 10:34:17 +0530

    refactor useCarousel

--- hooks/useCarousel.ts
+++ hooks/useCarousel.ts
@@ -1,25 +1,47 @@
 import { useKeenSlider } from 'keen-slider/react';
 import { useState } from 'react';
 
-import useCardCount from './useCardCount';
+import { TCardIndex } from '../utils/types';
+
+const getValueAtBp = (num: number, fun: (n: number) => void) => ({
+  slidesPerView: num,
+  mounted() {
+    fun(num);
+  },
+});
 
 const useCarousel = (isHero: boolean) => {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const { pos, slideCount } = useCardCount({ xs: 2, sm: 3, md: 4, lg: 5, xl: 7 });
+  const [slideCount, setSlideCount] = useState(2);
   const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
     initial: 0,
     duration: isHero ? 1200 : 2500,
     spacing: isHero ? 12 : 0,
-    slidesPerView: isHero ? 1 : slideCount,
+    slidesPerView: isHero ? 1 : 2,
     controls: true,
+    resetSlide: true,
     slideChanged(s) {
       setCurrentSlide(s.details().relativeSlide);
     },
-    breakpoints: {
-      '(min-width: 1024px)': { controls: isHero },
-      '(min-width: 1280px)': { controls: isHero },
-    },
+    ...(isHero
+      ? {}
+      : {
+          breakpoints: {
+            '(min-width: 0px)': getValueAtBp(2, setSlideCount),
+            '(min-width: 640px)': getValueAtBp(3, setSlideCount),
+            '(min-width: 768px)': getValueAtBp(4, setSlideCount),
+            '(min-width: 1024px)': { controls: false, ...getValueAtBp(5, setSlideCount) },
+            '(min-width: 1280px)': { controls: false, ...getValueAtBp(7, setSlideCount) },
+          },
+        }),
   });
+
+  const end = Array(25)
+    .fill(0)
+    .map((_, j) => (j + 1) * slideCount - 1);
+
+  const pos = (index: number): TCardIndex =>
+    index % slideCount === 0 ? 'first' : end.includes(index) ? 'last' : 'middle';
 
   const next = () => (isHero ? slider.next() : slider.moveToSlide(currentSlide + slideCount));
   const prev = () => (isHero ? slider.prev() : slider.moveToSlide(currentSlide - slideCount));
