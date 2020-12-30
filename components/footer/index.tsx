4f019883f2414bd473ecf4296366f3992a29f9bf
vishnumohanrk <mohanvish1999@gmail.com>
2020-12-30 17:36:55 +0530

    tmdb attribution

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,7 +4,7 @@ import ExternalLink from './ExternalLink';
 const AppFooter = () => (
   <footer className={`${classFull} text-sm flex justify-center md:justify-end space-x-4 mt-4 pb-8`}>
     <ExternalLink name="View On Github" src={githubLink} />
-    <ExternalLink name="Powered by TMDB" src="https://developers.themoviedb.org/3" />
+    <ExternalLink name="Data from TMDb" src="https://developers.themoviedb.org/3" />
   </footer>
 );
 
